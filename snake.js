2271ea2945c31856c11d3c8f0d9ee969e32c0899
Adam <adamwatters11@gmail.com>
2014-09-14 23:31:25 -0400

    canvas resized and colored, starting to implement blocks for walls

--- snake.js
+++ snake.js
@@ -4,7 +4,6 @@
 
     var screen = document.getElementById("screen").getContext("2d");
 
-
     this.size = { x: screen.canvas.width, y: screen.canvas.height };
     this.bodies = [new Player(this)];
 
@@ -30,7 +29,8 @@
     },
 
     draw: function(screen){
-      screen.clearRect(0, 0, this.size.x, this.size.y);
+      screen.fillStyle="#99FF66";
+      screen.fillRect(0,0,this.size.x,this.size.y);
       for (var i = 0; i < this.bodies.length; i++) {
         if (this.bodies[i].draw !== undefined) {
           this.bodies[i].draw(screen);
@@ -43,22 +43,49 @@
   this.game = game;
   this.size = { x: 15, y: 15 };
   this.center = { x: this.game.size.x / 2, y: this.game.size.y / 2 };
-}
+  this.keyboarder = new Keyboarder();
+
+  };
 
   Player.prototype = {
-    update: function() {
-        this.center.x += .5;
-        return true;
-      },
+    update: function(screen) {
+      if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
+          this.center.x -= 5;
+      } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
+          this.center.x += 5;
+      } else if (this.keyboarder.isDown(this.keyboarder.KEYS.UP)) {
+          this.center.y -= 5;
+      } else if (this.keyboarder.isDown(this.keyboarder.KEYS.DOWN)) {
+          this.center.y += 5;
+      }
+    },
 
     draw: function(screen) {
-        drawRect(screen, this);
-        return true;
-     }
+      drawRect(screen, this);
+    }
+  };
+
+
+  var Keyboarder = function() {
+    var keyState = {};
+    window.addEventListener('keydown', function(e) {
+      keyState[e.keyCode] = true;
+    });
+
+    window.addEventListener('keyup', function(e) {
+      keyState[e.keyCode] = false;
+    });
+
+    this.isDown = function(keyCode) {
+      return keyState[keyCode] === true;
+    };
+
+    this.KEYS = { LEFT: 37, RIGHT: 39, UP: 38, DOWN: 40};
   };
 
 
   var drawRect = function(screen, body) {
+    screen.fillStyle="black";
     screen.fillRect(body.center.x - body.size.x / 2,
                     body.center.y - body.size.y / 2,
                     body.size.x,
